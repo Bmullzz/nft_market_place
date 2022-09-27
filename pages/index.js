87b4b9a2467ed8b4e0f7e0d967be9eb94215b146
Brian Mullin <brmullin@email.msmary.edu>
2022-09-27 14:03:05 -0400

    updated eslint and gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Under Construction...
+    </h1>
+  </div>
+);
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Under Construction...
-      </h1>
-    </div>
-  )
-}
+export default Home;
