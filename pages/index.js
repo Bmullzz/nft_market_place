419fa4a1d2aca5cf87f56986a36cbbf58427578d
Brian Mullin <brmullin@email.msmary.edu>
2022-09-28 14:53:39 -0400

    add Banner component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { Banner } from '../components';
+
 const Home = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">
-      Under Construction...
-    </h1>
+  <div className="flex justify-center sm:px-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner />
+    </div>
   </div>
 );
 
