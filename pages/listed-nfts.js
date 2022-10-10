6c53981930a5007521ef498187258c2d8fa7301f
Brian Mullin <brmullin@email.msmary.edu>
2022-10-10 15:04:59 -0400

    implement fetchMyNFTsOrListedNFTs into listed-nfts page

--- pages/listed-nfts.js
+++ pages/listed-nfts.js
@@ -4,8 +4,17 @@ import { NFTContext } from '../context/NFTContext';
 import { Loader, NFTCard } from '../components';
 
 const ListedNFTs = () => {
+  const { fetchMyNFTsOrListedNFTs } = useContext(NFTContext);
   const [nfts, setNfts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchMyNFTsOrListedNFTs('fetchItemsListed')
+      .then((items) => {
+        setNfts(items);
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
     return (
@@ -15,13 +24,13 @@ const ListedNFTs = () => {
     );
   }
 
-  // if (!isLoading && nfts.length === 0) {
-  //   return (
-  //     <div className="flexCenter sm:p-4 p-16 min-h-screen">
-  //       <h1 className="font-poppins dark:text-white text-nft-black-1 text-3xl font-extrabold">No NFTs Listed for Sale</h1>
-  //     </div>
-  //   );
-  // }
+  if (!isLoading && nfts.length === 0) {
+    return (
+      <div className="flexCenter sm:p-4 p-16 min-h-screen">
+        <h1 className="font-poppins dark:text-white text-nft-black-1 text-3xl font-extrabold">No NFTs Listed for Sale</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="flex justify-center sm:px-4 p-12 min-h-screen">
