e5f447bca017a1392407387dd5602c4ccca5300b
Brian Mullin <brmullin@email.msmary.edu>
2022-10-11 14:04:03 -0400

    add Creator, seller address, and avatar to nft-details page

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -21,11 +21,29 @@ const NFTDetails = () => {
     setIsLoading(false);
   }, [router.isReady]);
 
+  if (isLoading) return <Loader />;
+
   return (
     <div className="relative flex justify-center md:flex-col min-h-screen">
       <div className="relative flex-1 flexCenter sm:px-4 p-12 border-r md:border-r-0 md:border-b dark:border-nft-black-1 border-nft-gray-1">
+        <div className="relative w-557 minmd:w-2/3 minmd:h-2/3 sm:w-full sm:h-300 h-557">
+          <Image src={nft.image} objectFit="cover" className="rounded-xl shadow-lg" layout="fill" />
+        </div>
+      </div>
+
+      <div>
         <div>
-          <Image src={nft.image} />
+          <h2>{nft.name}</h2>
+        </div>
+
+        <div>
+          <p>Creator</p>
+          <div>
+            <div>
+              <Image src={images.creator1} objectFit="cover" className="rounded-full" />
+            </div>
+            <p>{shortenAddress(nft.seller)}</p>
+          </div>
         </div>
       </div>
     </div>
