34a94a0b44ee43afac7f7d6bbab941282ec7856a
Brian Mullin <brmullin@email.msmary.edu>
2022-10-11 13:48:22 -0400

    add details and structure to nft-details

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import Image from 'next/Image';
+import Image from 'next/image';
 
 import { NFTContext } from '../context/NFTContext';
 import { Loader, NFTCard, Banner } from '../components';
--- pages/nft-details.js
+++ pages/nft-details.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-const NFTDetails = () => (
-  <div>NFTDetails</div>
-);
+import { NFTContext } from '../context/NFTContext';
+import { Loader, NFTCard, Button } from '../components';
+import { shortenAddress } from '../utils/shortenAddress';
+import images from '../assets';
+
+const NFTDetails = () => {
+  const { currentAccount } = useContext(NFTContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [nft, setNft] = useState({ image: '', tokenId: '', name: '', owner: '', price: '', seller: '' });
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    setNft(router.query);
+
+    setIsLoading(false);
+  }, [router.isReady]);
+
+  return (
+    <div className="relative flex justify-center md:flex-col min-h-screen">
+      <div className="relative flex-1 flexCenter sm:px-4 p-12 border-r md:border-r-0 md:border-b dark:border-nft-black-1 border-nft-gray-1">
+        <div>
+          <Image src={nft.image} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NFTDetails;
