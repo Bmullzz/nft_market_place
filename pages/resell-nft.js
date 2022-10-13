7e99a335a2d43c2f1c8e90cfc45ba59d064de392
Brian Mullin <brmullin@email.msmary.edu>
2022-10-13 18:57:13 -0400

    add logic to fetch data for resell

--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
-const ResellNFT = () => (
-  <div>ResellNFT</div>
-);
+import { NFTContext } from '../context/NFTContext';
+import { Loader, Button, Input } from '../components';
+
+const ResellNFT = () => {
+  const {} = useContext(NFTContext);
+  const router = useRouter();
+  const { tokenId, tokenURI } = router.query;
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+  
+  const fetchNFT = async () => {
+    const { data } = await axios.get(tokenURI);
+
+    setPrice(data.price);
+    setImage(data.image);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (tokenURI) fetchNFT();
+  }, [tokenURI]);
+
+  if (isLoading) {
+    return (
+      <div className="flexStart min-h-screen">
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+
+    </div>
+  );
+};
 
 export default ResellNFT;
